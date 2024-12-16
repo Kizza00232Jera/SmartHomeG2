fa4b00943ae4c260b5f9930506b3483b02ff172b
Kizza00232Jera <antonio.jera10@gmail.com>
2024-12-16 11:57:40 +0100

    tailwindcss

---
+++ app/index.tsx
@@ -0,0 +1,10 @@
+import { Text, View } from "react-native";
+import "../global.css";
+
+export default function HomeScreen() {
+  return (
+   <View className="flex-1 justify-center">
+    <Text className="text-red-500 underline">Hello World</Text>
+   </View>
+  );
+}
